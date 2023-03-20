1abb31b0b9aa8616f8dd98ccf0cba8930daebe6d
tph <thousden053@outlook.com>
2023-03-20 04:10:24 +0000

    updates ch names

--- fileadd.sh
+++ fileadd.sh
@@ -3,8 +3,8 @@
 for (( i=5; i<=14; i++ ))
 do
     if [ $i -lt 10 ]; then
-        touch challenge_0$i.sh
+        touch Challenge_0$i.sh
     else
-        touch challenge_$i.sh
+        touch Challenge_$i.sh
     fi
 done
