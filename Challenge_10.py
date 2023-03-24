e62849eabb5518342f5e1e3bb2cca4567e03398d
tph <thousden053@outlook.com>
2023-03-24 16:02:15 +0000

    submit Ch10

--- Challenge_10.py
+++ Challenge_10.py
@@ -1,5 +1,21 @@
 #!/usr/bin/env python3
 
-# Script:         Ops 301 Ops Chall 07
+# Script:         Ops 301 Ops Chall 10
 # Purpose:        
 # Why:     
+
+
+# Using file handling commands, create a Python script that creates a new .txt file, appends three lines, prints to the screen the first line, then deletes the .txt file.
+#Main
+
+
+newfile = open("test.txt", "w")
+
+# How to open a file
+openfile = open("test.txt")
+
+# How to return the five first characters of a file
+read = open("test.txt", "r")
+print("test.txt"(readline(1))
+
+#Main
